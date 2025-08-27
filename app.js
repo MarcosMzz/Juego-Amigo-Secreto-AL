bfcc0dd82869e8d822ddb1c095c2fe858e18262d
Marcos <marcossantinomazzanti@gmail.com>
2025-08-27 18:48:10 -0300

    Se agrego funcion que evita que puedas escribir espacios o numeros en el input, se agrego funcion que limpiar la caja del input y  ahora la lista de amigos se hace con objetos li, y se agregan horizontalmente

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ let cantidadAmigos = 0;
 function agregarAmigo() {
     let nombreAmigo = document.getElementById("amigo").value;
     if(listaNombres.includes(nombreAmigo) || esNuloOESpacioEnBlanco(nombreAmigo)){
-        asignarTextoElemento("informacion_lista", "Por favor ingrese un amigo distinto o no ingrese un espacio en blanco")
+        asignarTextoElemento("informacion_lista", "Por favor ingrese un amigo distinto o no ingrese un espacio en blanco");
     }else{
-        listaNombres.push(nombreAmigo);
-        asignarTextoElemento("informacion_lista", "Amigo " + nombreAmigo + " Agregado con exito")
-        asignarTextoElemento("listaAmigos", listaNombres)
+      listaNombres.push(nombreAmigo);
+        asignarTextoElemento("informacion_lista", "Amigo " + nombreAmigo + " Agregado con exito");
+        crearLi(listaNombres);
+        limpiarCaja();
     }
 }
 
@@ -22,27 +23,61 @@ function asignarTextoElemento(elemento, texto){
 
 function mostrarListaAmigos(){
     asignarTextoElemento(listaAmigos, listaNombres);
+    return;
 }
 
 function esNuloOESpacioEnBlanco(amigo) {
-  //Nos fijamos si es null o undefined
-  if (amigo == null) {
-    return true;
-  }
+    //Nos fijamos si es null o undefined
+    if (amigo == null) {
+      return true;
+    }
 
-  // Si es un string, trimeala para fijar si esta vacia
-  if (typeof amigo === 'string') {
-    return amigo.trim().length === 0;
-  }
+    // Si es un string, trimeala para fijar si esta vacia
+    if (typeof amigo === 'string') {
+      return amigo.trim().length === 0;
+    }
 
-  //Si es no nula/undefined y no una string, considerala que no nulla o que no tiene espacios en blanco
-  return false;
+    //Si es no nula/undefined y no una string, considerala que no nulla o que no tiene espacios en blanco
+    return false;
 }
 
 function sortearAmigo(){
     let numeroAmigoSorteado = Math.floor(Math.random()*listaNombres.length);
-    console.log(numeroAmigoSorteado);
-    console.log(listaAmigos.length);
+    
     let amigoSorteado = listaNombres[numeroAmigoSorteado];
-    asignarTextoElemento("resultado", "Felicidades, " + amigoSorteado + ", fuiste el amigo sorteado");
+    //asignarTextoElemento("resultado", "Felicidades, " + amigoSorteado + ", fuiste el amigo sorteado");
+    let lista = document.getElementById("resultado");
+  asignarTextoElemento("resultado", "");
+    let nuevoElementoLista = document.createElement("li");
+    nuevoElementoLista.textContent = amigoSorteado;
+    lista.appendChild(nuevoElementoLista);
+    return;
+
 }
+
+function crearLi(listaDeAmigos){
+  let lista = document.getElementById("listaAmigos");
+  asignarTextoElemento("listaAmigos", "");
+
+  for(let amigo of listaDeAmigos){
+    let nuevoElementoLista = document.createElement("li");
+    nuevoElementoLista.textContent = amigo;
+    lista.appendChild(nuevoElementoLista);
+  }
+  return;
+}
+
+function limpiarCaja() {
+  document.querySelector("#amigo").value = "";
+  return;
+}
+
+  //Codigo para que el input solo puedan ser letras(No admite espacios ni numeros)
+  document.addEventListener('DOMContentLoaded', function() {
+    const inputField = document.getElementById('amigo');
+
+    inputField.addEventListener('input', function(event) {
+      // Replace any character that is NOT a letter (a-z, A-Z) with an empty string
+      event.target.value = event.target.value.replace(/[^a-zA-Z]/g, '');
+    });
+  });
